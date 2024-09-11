fd8d46574d02a3989a1e7ee03d487ba3dcb54d56
william-saputra <williammatthews.id@gmial.com>
2024-09-11 16:17:16 +0700

    another commit tutor 2

--- main/tests.py
+++ main/tests.py
@@ -23,4 +23,5 @@ class mainTest(TestCase):
           feelings = "senang sih, cuman tadi baju aku basah kena hujan :(",
           mood_intensity = 8,
         )
-        self.assertTrue(mood.is_mood_strong)
+        self.assertTrue(mood.is_mood_strong) #test
+        
